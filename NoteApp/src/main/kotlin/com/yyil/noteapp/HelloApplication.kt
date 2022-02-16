9c44924a61d4a007a5ec2fedc370ddffc823bbdd
unknown <y84qiu@uwaterloo.ca>
2022-02-15 23:39:17 -0500

    WE'RE BACK TO HTML BABY

--- NoteApp/src/main/kotlin/com/yyil/noteapp/HelloApplication.kt
+++ NoteApp/src/main/kotlin/com/yyil/noteapp/HelloApplication.kt
@@ -6,6 +6,10 @@ import javafx.application.Application
 import javafx.scene.Scene
 import javafx.stage.Stage
 
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import java.io.File
+
 
 class HelloApplication : Application() {
 
@@ -16,9 +20,16 @@ class HelloApplication : Application() {
     }
 
     override fun start(stage: Stage) {
+        val webView = WebView()
+        val webEngine = webView.engine
+
+        val url = javaClass.classLoader.getResource("editor.html").toExternalForm()
+
+        webEngine.load(url)
+
         val component = UiComponent()
         component.init()
-        stage.scene = Scene(component.base, WindowSettings.width, WindowSettings.height)
+        stage.scene = Scene(webView, WindowSettings.width, WindowSettings.height)
         stage.title = WindowSettings.title
         //stage.initStyle(StageStyle.UNDECORATED)
         stage.show()
