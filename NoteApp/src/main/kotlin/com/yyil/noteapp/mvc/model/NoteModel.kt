87f9d6df22fd3560f6d51f8dfec21e719715e730
Logan <lmaier@uwaterloo.ca>
2022-03-09 02:55:18 -0500

    rebasing reverted to my old file structure, but needed to do it anyways in order to properly test the new regex based functions

--- NoteApp/src/main/kotlin/com/yyil/noteapp/mvc/model/NoteModel.kt
+++ NoteApp/src/main/kotlin/com/yyil/noteapp/mvc/model/NoteModel.kt
@@ -5,48 +5,17 @@ class NoteModel {
     //val lables = mapOf<>()
 
     fun insertAnnotation (annotation: String, selection: String): String {
-        var inTag = false
-        var startTag = false
-        var endTag = false
         val openTag = "<span title=\"$annotation\">"
         val closeTag = "</span>"
-        var result = ""
+        var result = selection.replace(">(?=[^<])".toRegex(), ">$openTag")
+            .replace("(?<=[^>])<".toRegex(), "$closeTag<")
 
         if (selection[0] != '<') {
-            result += openTag
-        }
-
-        for (letter in selection) {
-
-            if (letter == '<') {
-                inTag = true
-                startTag = false
-
-                if (endTag) {
-                    result += closeTag
-                    endTag = false
-                }
-            }
-
-            if (startTag) {
-                result += openTag
-                startTag = false
-            }
-
-            result += letter
-
-            if (!inTag) {
-                endTag = true
-            }
-
-            else if (letter == '>') {
-                inTag = false
-                startTag = true
-            }
+            result = "$openTag$result"
         }
 
         if (selection[selection.length - 1] != '>') {
-            result += closeTag
+            result = "$result$closeTag"
         }
 
         return result
