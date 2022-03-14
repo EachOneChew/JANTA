eccc5bc954cfd9ab798199250a6e17bd61354236
Yixin Li <35883134+EL369@users.noreply.github.com>
2022-03-14 11:17:10 -0400

    Test text disable horizontal scroll

--- NoteApp/src/main/kotlin/com/yyil/noteapp/mvc/view/NoteArea.kt
+++ NoteApp/src/main/kotlin/com/yyil/noteapp/mvc/view/NoteArea.kt
@@ -2,6 +2,7 @@ package com.yyil.noteapp.mvc.view
 
 import com.yyil.noteapp.constant.ComponentConstant
 import javafx.scene.control.ScrollPane
+import javafx.scene.control.TextArea
 import javafx.scene.layout.HBox
 import javafx.scene.layout.Priority
 import javafx.scene.layout.VBox
@@ -11,7 +12,7 @@ import javafx.scene.web.WebView
 class NoteArea(tinyMCEView: WebView) : View {
     override val base = VBox()
 
-    val testTextSync = Text()
+    val testTextSync = TextArea()
     private val testTextScroll = ScrollPane(testTextSync)
 
     init {
@@ -20,10 +21,11 @@ class NoteArea(tinyMCEView: WebView) : View {
         testTextScroll.maxHeight = ComponentConstant.TEST_SCROLL_PANE_HEIGHT
         testTextScroll.minHeight = ComponentConstant.TEST_SCROLL_PANE_HEIGHT
         testTextScroll.isFitToWidth = true
-        testTextScroll.isFitToHeight = true
+        testTextScroll.hbarPolicy = ScrollPane.ScrollBarPolicy.NEVER
+        testTextSync.wrapTextProperty().set(true)
 
         base.children.add(tinyMCEView)
-        base.children.add(testTextScroll)
+        base.children.add(testTextSync)
         HBox.setHgrow(base, Priority.ALWAYS)
     }
 }
