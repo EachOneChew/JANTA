6bba72e99a9adf7ab2473b487f6bad74853c02c5
Yixin Li <35883134+EL369@users.noreply.github.com>
2022-03-23 00:49:42 -0400

    Rearrange code to fit mvc model

---
+++ NoteApp/src/main/kotlin/com/yyil/noteapp/mvc/controller/NoteEditMenuController.kt
@@ -0,0 +1,38 @@
+package com.yyil.noteapp.mvc.controller
+
+import com.yyil.noteapp.mvc.model.Model
+import com.yyil.noteapp.mvc.view.NoteEditMenu
+import com.yyil.noteapp.mvc.view.NoteRepository
+import javafx.event.EventHandler
+
+class NoteEditMenuController(
+    var model: Model,
+    noteEditMenu: NoteEditMenu,
+    noteRepository: NoteRepository
+) {
+    init {
+
+        noteEditMenu.noteCell.emptyProperty().addListener { _, _, isEmpty ->
+            if(isEmpty){
+                noteEditMenu.noteCell.contextMenu = null
+            }else{
+                noteEditMenu.noteCell.contextMenu = noteEditMenu
+            }
+        }
+
+        noteEditMenu.deleteOption.onAction = EventHandler {
+            println("Deleted note ${noteEditMenu.noteCell.note.title};" +
+                    " ID: ${noteEditMenu.noteCell.note.id}")
+            model.notes.remove(noteEditMenu.noteCell.note)
+        }
+
+        noteEditMenu.renameOption.onAction = EventHandler {
+            println("Rename note ${noteEditMenu.noteCell.note.title};" +
+                    " ID: ${noteEditMenu.noteCell.note.id}")
+            noteRepository.renameDialog.showAndWait()
+            noteEditMenu.noteCell.note.title = noteRepository.renameDialog.editor.text
+            noteRepository.noteList.refresh()
+            println(" To ${noteEditMenu.noteCell.note.title}")
+        }
+    }
+}
--- NoteApp/src/main/kotlin/com/yyil/noteapp/mvc/controller/NoteRepositoryController.kt
+++ NoteApp/src/main/kotlin/com/yyil/noteapp/mvc/controller/NoteRepositoryController.kt
@@ -2,11 +2,10 @@ package com.yyil.noteapp.mvc.controller
 
 import com.yyil.noteapp.mvc.model.Model
 import com.yyil.noteapp.mvc.view.NoteCell
+import com.yyil.noteapp.mvc.view.NoteEditMenu
 import com.yyil.noteapp.mvc.view.NotePlaceHolderCell
 import com.yyil.noteapp.mvc.view.NoteRepository
 import javafx.event.EventHandler
-import javafx.scene.control.ContextMenu
-import javafx.scene.control.MenuItem
 
 class NoteRepositoryController(
     val model: Model,
@@ -16,7 +15,7 @@ class NoteRepositoryController(
         noteRepository.noteList.onMouseClicked = EventHandler {
             val i = noteRepository.noteList.selectionModel.selectedIndex
             model.handleNoteSelect(i)
-            println("Selected note $i")
+            println("Selected note index $i")
         }
 
         noteRepository.noteList.items = model.notes
@@ -26,44 +25,12 @@ class NoteRepositoryController(
         noteRepository.noteList.setCellFactory {
 
             var noteCell = NoteCell()
-            println("Created note cell ${noteCell.note.title}; ID: ${noteCell.note.id}")
-            var contextMenu = ContextMenu()
-            var renameOption = MenuItem("Rename")
-            var deleteOption = MenuItem("Delete")
-            contextMenu.items.addAll(renameOption, deleteOption)
-            
-            noteCell.emptyProperty().addListener { observable, wasEmpty, isEmpty ->
-                if(isEmpty){
-                    noteCell.contextMenu = null
-                }else{
-                    noteCell.contextMenu = contextMenu
-                }
-            }
 
-            deleteOption.onAction = EventHandler {
-                println("Deleted note ${noteCell.note.title}; ID: ${noteCell.note.id}")
-                model.notes.remove(noteCell.note)
-            }
-
-            renameOption.onAction = EventHandler {
-                println("Rename note ${noteCell.note.title}; ID: ${noteCell.note.id}")
-                noteRepository.renameDialog.showAndWait()
-                noteCell.note.title = noteRepository.renameDialog.editor.text
-                noteRepository.noteList.refresh()
-                println(" To ${noteCell.note.title}")
-            }
+            var noteEditMenu = NoteEditMenu(noteCell)
+            NoteEditMenuController(model, noteEditMenu, noteRepository)
 
             noteCell
         }
-        /*
-        noteRepository.noteList.onMouseClicked = EventHandler<MouseEvent> { click ->
-            if (click.isSecondaryButtonDown) {
-                val selectedNote = noteRepository.noteList.selectionModel.selectedItem
-
-            }
-        }
-
-         */
     }
 }
 
