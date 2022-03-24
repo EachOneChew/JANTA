15846d03a94882ef176915987690792b1b5f9a78
Yichuan Qiu <y84qiu@uwaterloo.ca>
2022-03-24 14:51:55 -0400

    Label references working

--- NoteApp/src/main/resources/application_script.js
+++ NoteApp/src/main/resources/application_script.js
@@ -185,8 +185,8 @@ window.initFunction = function (initContent) {
                     {
                         type: "htmlpanel",
                         html: `
-                            <p>Selections may be annotated with a short text. The annotation will then be displayed when the user hovers over the affected selection.</p>
-                            <p>Deletion and editing of annotations is done through the context form triggered by moving the caret into their range.</p>
+                            <p>Selections may be annotated with a short text. The <span style="background-color: #FFFF00">annotation</span> will then be displayed when the user hovers over the affected selection.</p>
+                            <p>Deletion and editing of <span style="background-color: #FFFF00">annotations</span> is done through the context form triggered by moving the caret into their range.</p>
                         `,
                     },
                 ],
@@ -198,12 +198,12 @@ window.initFunction = function (initContent) {
                     {
                         type: "htmlpanel",
                         html: `
-                            <p>Within a note, selections may be designated label targets and label references. Label references point to label targets.</p>
-                            <p>Note that references are not allowed to point to targets that do not exist. Target uniqueness is enforced by the editor.</p>
-                            <p>Label targets are given a type, then a name by the user on creation through two successive context forms. References are pointed to targets using names.</p>
-                            <p>To see the name of a target, hover over its selection.</p>
+                            <p>Within a note, selections may be designated label <span style="background-color: #FF8C00">targets</span> and label <span style="background-color: #7FFFD4">references</span>. Label <span style="background-color: #7FFFD4">references</span> point to label <span style="background-color: #FF8C00">targets</span>.</p>
+                            <p>Note that <span style="background-color: #7FFFD4">references</span> are not allowed to point to <span style="background-color: #FF8C00">targets</span> that do not exist. <span style="background-color: ##FF8C00">Target</span> uniqueness is enforced by the editor.</p>
+                            <p>Label <span style="background-color: #FF8C00">targets</span> are given a type, then a name by the user on creation through two successive context forms. <span style="background-color: #7FFFD4">references</span> are pointed to <span style="background-color: #FF8C00">targets</span> using names.</p>
+                            <p>To see the name of a <span style="background-color: ##FF8C00">target</span>, hover over its selection.</p>
                             <p>There is a tab in the sidebar which allows users to aggregate labels within a note by type.</p>
-                            <p>Deleting a target will delete all references pointing to it. Deleting a reference will not affect the target.</p>
+                            <p>Deleting a <span style="background-color: ##FF8C00">target</span> will delete all <span style="background-color: #7FFFD4">references</span> pointing to it. Deleting a <span style="background-color: #7FFFD4">reference</span> will not affect the <span style="background-color: ##FF8C00">target</span>.</p>
                             <p>Deletion and editing of labels is done through the context form triggered by moving the caret into their range.</p>
                         `,
                     },
@@ -408,9 +408,11 @@ window.initFunction = function (initContent) {
                                     ).contentDocument;
                                 doc.querySelectorAll(
                                     `a[href="#${temp}"]`
-                                ).forEach((e) =>
-                                    ed.formatter.remove("labelref", null, e)
-                                );
+                                ).forEach((e) => {
+                                    e.removeAttribute("href");
+                                    e.removeAttribute("style");
+                                    ed.formatter.remove("labelref", null, e);
+                                });
                                 // CALL MODEL DELETE
                                 // callModel(...)
                             }
@@ -466,6 +468,66 @@ window.initFunction = function (initContent) {
                             clearLabeltargetTippys();
                             setLabeltargetTippys();
                             window.labelTypeVar = null;
+                            formApi.hide();
+                        },
+                    },
+                ],
+            });
+
+            ed.ui.registry.addContextForm("labelref-form", {
+                label: "Label reference",
+                initValue: function () {
+                    var root = getRoot(
+                        ed.selection.getNode(),
+                        "labelref_class"
+                    );
+                    return root ? root.getAttribute("href").substring(1) : "";
+                },
+                position: "selection",
+                predicate: calcLabelrefPredicate,
+                commands: [
+                    {
+                        type: "contextformbutton",
+                        icon: "checkmark",
+                        tooltip: "Add reference",
+                        primary: true,
+                        onAction: function (formApi) {
+                            var value = formApi.getValue();
+                            if (value) {
+                                var root = getRoot(
+                                    ed.selection.getNode(),
+                                    "labelref_class"
+                                );
+                                if (!root) {
+                                    for (k in ed.formatter.get()) {
+                                        ed.formatter.remove(k);
+                                    }
+                                    ed.formatter.apply("labelref", {
+                                        value: `#${value}`,
+                                    });
+                                } else {
+                                    root.setAttribute("href", `#${value}`);
+                                }
+                            }
+                            ed.fire("Change");
+                            formApi.hide();
+                        },
+                    },
+                    {
+                        type: "contextformbutton",
+                        icon: "close",
+                        tooltip: "Remove reference",
+                        onAction: function (formApi) {
+                            var root = getRoot(
+                                ed.selection.getNode(),
+                                "labelref_class"
+                            );
+                            if (root) {
+                                root.removeAttribute("href");
+                                root.removeAttribute("style");
+                                ed.formatter.remove("labelref", null, root);
+                            }
+                            ed.fire("Change");
                             formApi.hide();
                         },
                     },
